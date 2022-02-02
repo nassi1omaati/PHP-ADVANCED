d71df596dfc7d8ece387e6f4dc0b1dcbcc038ada
Nassim1Omaati <2151890@talnet.nl>
2022-02-02 13:40:45 +0100

    opdracht 2 af

--- 1-Arrays/taak02/index.php
+++ 1-Arrays/taak02/index.php
@@ -1,9 +1,6 @@
 <?php
 
-$lengte = array("Piet"=>187, "Muhammad"=>184, "Els"=>168);
-$klas = array("Piet"=>"8G", "Muhammad"=>"9G", "Els"=>"7G");
-$leeftijd = array("Piet"=>17, "Muhammad"=>17, "Els"=>19);
-$achternaam = array("Piet"=>"de vries", "Muhammad"=>"Ali", "Els"=>"bakker");
-$voornaam = array("Piet"=>"Piet", "Muhammad"=>"Muhammad", "Els"=>"Els");
+ $Willem = array("Voornaam" => "Willem", "Achternaam" => "Alexander", "Leeftijd" => 53, "Klas" => "9G");
 
-echo "Gegevens Muhammad: Voornaam: " . $voornaam["Muhammad"] . " Achternaam: ".$achternaam["Muhammad"] . " Leeftijd: " . $leeftijd["Muhammad"] . " Klas: " . $klas["Muhammad"];
+
+ echo "Gegevens Muhammad: Voornaam: " . $Willem["Voornaam"] . " Achternaam: ".$Willem["Achternaam"] . " Leeftijd: " . $Willem["Leeftijd"] . " opleiding: " . $Willem["Software Ontwikkeling"];
