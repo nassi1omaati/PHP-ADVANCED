cc5e6b22e193e1506ba21737f1ec12c88d5823d7
Nassim1Omaati <2151890@talnet.nl>
2022-02-02 11:49:51 +0100

    opdr 1 af

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$lengte = array("Piet"=>187, "Muhammad"=>184, "Els"=>168);
+$klas = array("Piet"=>"8G", "Muhammad"=>"9G", "Els"=>"7G");
+$leeftijd = array("Piet"=>17, "Muhammad"=>17, "Els"=>19);
+$achternaam = array("Piet"=>"de vries", "Muhammad"=>"Ali", "Els"=>"bakker");
+$voornaam = array("Piet"=>"Piet", "Muhammad"=>"Muhammad", "Els"=>"Els");
+
+// $muhammad = array("Voornaam" => "Muhammad", "Achternaam" => "Ali", "Leeftijd" => 17, "Klas" => "9G");
+
+
+echo "Gegevens Muhammad: Voornaam: " . $voornaam["Muhammad"] . " Achternaam: ".$achternaam["Muhammad"] . " Leeftijd: " . $leeftijd["Muhammad"] . " Klas: " . $klas["Muhammad"];
+// echo "Gegevens Muhammad: Voornaam: " . $muhammad["Voornaam"] . " Achternaam: ".$achternaam["Muhammad"] . " Leeftijd: " . $leeftijd["Muhammad"] . " Klas: " . $klas["Muhammad"];
