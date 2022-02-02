938d9d9f3fcd6e686676c9821191cc6d8cd0111e
Nassim1Omaati <2151890@talnet.nl>
2022-02-02 13:32:56 +0100

    opdracht 1 opnieuw

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$lengte = array("Piet"=>187, "Muhammad"=>184, "Els"=>168);
+$klas = array("Piet"=>"8G", "Muhammad"=>"9G", "Els"=>"7G");
+$leeftijd = array("Piet"=>17, "Muhammad"=>17, "Els"=>19);
+$achternaam = array("Piet"=>"de vries", "Muhammad"=>"Ali", "Els"=>"bakker");
+$voornaam = array("Piet"=>"Piet", "Muhammad"=>"Muhammad", "Els"=>"Els");
+
+echo "Gegevens Muhammad: Voornaam: " . $voornaam["Muhammad"] . " Achternaam: ".$achternaam["Muhammad"] . " Leeftijd: " . $leeftijd["Muhammad"] . " Klas: " . $klas["Muhammad"];
