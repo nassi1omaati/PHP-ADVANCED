47ab0b4955e5fccc89e9167f076f2eca778cb41b
Nassim1Omaati <2151890@talnet.nl>
2022-02-03 14:32:50 +0100

    .

--- 2-Loops/taak01/index.php
+++ 2-Loops/taak01/index.php
@@ -1,10 +1,9 @@
 <?php
-
-for($b = 0; $b <= 18; $b++ ){
     echo '<ul>';
+for($b = 0; $b <= 18; $b++ ){
+
     echo  "<li>ik ben". " " . $b. "jaar, dus ik mag nog niet stemmen</li>";
 
 }
-echo '<ul>';
 echo  "<li>ik ben 18 jaar oud en heb dus stemrecht!</li>";
-eho '</ul>';
+echo '</ul>';
