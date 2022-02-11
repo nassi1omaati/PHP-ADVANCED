426a1e208fd2f1248d7dc3849b7c217bc65060c1
Nassim1Omaati <2151890@talnet.nl>
2022-02-11 13:24:03 +0100

    klaar

--- 2-Loops/taak04/loops-foreach-deel2.php
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -4,29 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="taak4.css">
+    <title></title>
 </head>
 <body>
-
-<form class= "Form" method="GET" action="JEBEM.php">
-    <label>klassenformulier</label> <br>
+    
+<form class= "Form" method="get" action="LF2.php">
+    <label>klassen</label> <br>
     <label>Ik zit in klas:</label> 
-<select id="klas" name="klas">
+<select id="school" name="school">
+
 <?php
-$Klas = "8B";
-$klassen = ['8A' , '8B' , '8C' , '8D' , '8E' , '9A' , '9B' , '9C' , '9D' , '9E'];
-foreach($klassen as $klas){
-    if($Klas == $klas){
+$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+foreach ($klassen as $klas){
+    if($Klas = $klas){
         echo "<option value='$Klas' selected>$klas</option>";
     }
     else{
         echo "<option value='$Klas'>$klas</option>";
     }
 }
+
 ?>
 </select> <br>
-<button class= "btn">Verzend</button>
+
+<button class= "button">Verzend</button>
 </form>
 
 </body>
