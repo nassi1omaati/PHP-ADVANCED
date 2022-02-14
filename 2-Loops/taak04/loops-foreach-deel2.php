99d8b430ccd839c6862313c5b8e887ef4b908bdc
Nassim1Omaati <2151890@talnet.nl>
2022-02-14 14:38:44 +0100

    2.4

--- 2-Loops/taak04/loops-foreach-deel2.php
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -1,35 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="taak4.css">
-    <title></title>
-</head>
-<body>
-    
-<form class= "Form" method="get" action="LF2.php">
-    <label>klassen</label> <br>
-    <label>Ik zit in klas:</label> 
-<select id="school" name="school">
-
-<?php
-$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
-foreach ($klassen as $klas){
-    if($Klas = $klas){
-        echo "<option value='$Klas' selected>$klas</option>";
-    }
-    else{
-        echo "<option value='$Klas'>$klas</option>";
-    }
-}
-
-?>
-</select> <br>
-
-<button class= "button">Verzend</button>
+<html>
+<center;>
+<form action="verzend.php">
+    <label for="klas">ik zit in klas:</label>
+<select>
+    <?php 
+    $klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+    foreach ($klassen as $klas) {
+     ?>
+    <option><?php echo $klas ?> </option>
+    <?php } ?>
+ </select>
+  <input type="submit" name="submit" value="verzenden">  
 </form>
-
+</center;>
 </body>
 </html>
---
+++ 2-Loops/taak04/verzend.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<?php 
+
+$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+
+echo "<ul>";
+foreach($klassen as $value){
+    echo"<li>$value</li>";
+}
+echo "<ul>";
+
+echo"<br>";
+
+echo "Succes met je opleiding "
+
+?>;
+
+
+
+</body>
+</html>
