73aaa34c0c2eababba4af52c5ec7db37256a973f
Nassim1Omaati <2151890@talnet.nl>
2022-03-01 13:00:53 +0100

    3.3

---
+++ 3-Forms/taak03/nummer.php
@@ -0,0 +1,6 @@
+<?php
+foreach($_GET as $key => $value){
+    echo $value . "<br>"; 
+}
+/*http://localhost/PHP-ADVANCED/3-Forms/taak03/nummer.php?artist=kid+cudi&titel=mr+rager&year=2010&genre=rock*/
+?>
